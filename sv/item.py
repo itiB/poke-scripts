d90f8a904045c32b879798c583787786cd4211d6
itiB <is0312vx@ed.ritsumei.ac.jp>
2022-12-19 20:31:39 +0900

    Add: auto item greaten

---
+++ sv/item.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+import argparse
+import serial
+from time import sleep
+import datetime
+
+"""
+COMMAND:
+    `$ python3 sender.py COM??`
+    ?? はデバイスマネージャで確認(USB Serial Portとして出てくる)
+
+SETING:
+    0. 'pip3 install pyserial'
+    1. @PowerShell: Find out which port to connect. `usbipd wsl list`
+    2. `usbipd wsl attach --busid <BUSID> --distribution docker-desktop`
+    3. Search connected USB list from`lsusb`
+    Finaly: `usbipd wsl detach --busid <busid>`
+
+USAGE:
+    1. コライドンを手持ちの上から2番目に置いて6匹埋める
+    2. ライド選択してライドフォルムに戻す
+    3. ボックスを開いてボックス1に移動
+    4. コマンドの実行 `python3 ./sv/item.py /dev/ttyUSB0 --loop 10`
+"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument('port')
+parser.add_argument('-l', '--loop', type=int, default=1)
+args = parser.parse_args()
+
+def send(msg, duration=0):
+    now = datetime.datetime.now()
+    print(f'{now}: {msg}')
+    ser.write(f'{msg}\r\n'.encode('utf-8'))
+    sleep(duration)
+    ser.write(b'RELEASE\r\n')
+
+ser = serial.Serial(args.port, 9600)
+sleep(2)
+send('Button LCLICK', 0.1)
+
+def main():
+    count = 0
+
+    try:
+        for _ in range(0, args.loop):
+            send('Button X', 0.1)
+            sleep(0.1)
+            send('Button X', 0.1)
+            sleep(0.1)
+            send('Button L', 0.1)
+            sleep(0.2)
+
+            # Select Koraidon
+            send('Button A', 0.1)
+            sleep(0.2)
+            send('LY MIN', 0.1)
+            sleep(0.1)
+            send('LY MIN', 0.1)
+            sleep(0.1)
+            send('LY MIN', 0.1)
+            sleep(0.1)
+            send('Button A', 0.1)
+            sleep(0.4)
+            # send('Button B', 0.1)
+            # sleep(0.1)
+
+            count += 1
+            print("  counter: {}/{}".format(count, args.loop))
+
+            # Close box
+            send('Button B', 0.1)
+            sleep(3.2)
+
+            # Change to Ride form
+            send('LX MIN', 0.1)
+            sleep(0.3)
+            send('Button A', 0.1)
+            sleep(0.2)
+            send('LY MIN', 0.1)
+            sleep(0.1)
+            send('LY MIN', 0.1)
+            sleep(0.1)
+            send('Button A', 0.1)
+            sleep(0.7)
+            send('Button A', 0.1)
+            sleep(0.2)
+            send('Button A', 0.1)
+            sleep(2.5)
+            send('Button A', 0.1)
+            sleep(0.7)
+
+            # Open Box
+            send('LX MAX', 0.1)
+            sleep(0.3)
+            send('LY MAX', 0.1)
+            sleep(0.1)
+            send('LY MAX', 0.1)
+            sleep(0.1)
+            send('Button A', 0.1)
+            sleep(3.0)
+
+    except KeyboardInterrupt:
+        print('Bye...')
+        ser.close()
+
+if __name__ == "__main__":
+    main()
